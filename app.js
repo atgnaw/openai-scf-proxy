d40b2594168ae063f6c818c5a3ae6503c4b08bf2
atgnaw <128681516+atgnaw@users.noreply.github.com>
2024-06-25 13:53:42 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const {
-  createProxyMiddleware
-} = require('http-proxy-middleware');
-const app = express()
-const port = 9000
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-app.use('/', createProxyMiddleware({
+// 创建第一个应用实例，监听端口9000
+const app1 = express();
+const port1 = 9000;
+
+app1.use('/', createProxyMiddleware({
   target: 'https://api.openai.com',
   changeOrigin: true,
   onProxyReq: (proxyReq, req, res) => {
@@ -18,6 +18,27 @@ app.use('/', createProxyMiddleware({
   }
 }));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app1.listen(port1, () => {
+  console.log(`Proxy to OpenAI API listening at http://localhost:${port1}`);
+});
+
+// 创建第二个应用实例，监听端口9001
+const app2 = express();
+const port2 = 9100;
+
+app2.use('/', createProxyMiddleware({
+  target: 'https://api.anthropic.com',
+  changeOrigin: true,
+  onProxyReq: (proxyReq, req, res) => {
+    // 移除 'x-forwarded-for' 和 'x-real-ip' 头，以确保不传递原始客户端 IP 地址等信息
+    proxyReq.removeHeader('x-forwarded-for');
+    proxyReq.removeHeader('x-real-ip');
+  },
+  onProxyRes: function (proxyRes, req, res) {
+    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+  }
+}));
+
+app2.listen(port2, () => {
+  console.log(`Proxy to Anthropic API listening at http://localhost:${port2}`);
+});
